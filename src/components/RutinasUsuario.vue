77fe4891585705768ab3ba11bcac62335ac8ff0f
Robin <105683782+rbncode@users.noreply.github.com>
2024-10-22 16:55:57 -0300

    feat: agrega condicionales y routerlink
    Se agregan condicionales para la vision de información y routerlinks para redireccionamiento de vistas

--- src/components/RutinasUsuario.vue
+++ src/components/RutinasUsuario.vue
@@ -48,9 +48,14 @@ export default {
 <template>
   <div v-if="rutinas.length == 0">
     <h3>Aún no tienes rutinas, ¡Agrega una rutina!</h3>
-    <RouterLink to="/agregar-rutinas"></RouterLink>
+    <RouterLink to="/agregar-rutinas">Agregar Rutina</RouterLink>
   </div>
-  <div v-else class="container"></div>
+  <div v-else class="container">
+    <RouterLink to="/agregar-rutinas">Agregar Rutina</RouterLink>
+    <div v-for="(rutina, index) in rutinas" :key="index">
+      <TarjetaRutina :rutina="rutina" />
+    </div>
+  </div>
 </template>
 
 <style></style>
