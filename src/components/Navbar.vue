c2c6cb42030efdd1e1064691c5d55b15531a8474
Robin <105683782+rbncode@users.noreply.github.com>
2024-10-22 18:16:33 -0300

    feat: agrega html a componente
    Se agrega el html del componente Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">
+      <h1>FitRoutine</h1>
+    </div>
+    <ul class="nav-links">
+      <li><RouterLink to="/">Inicio</RouterLink></li>
+      <li><RouterLink to="/">Perfil</RouterLink></li>
+      <li><RouterLink to="/rutinas">Rutinas</RouterLink></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle">Ejercicios</a>
+        <ul class="dropdown-menu">
+          <li>
+            <RouterLink to="/">Tren Superior</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/">Tren Inferior</RouterLink>
+          </li>
+          <li><RouterLink to="/">Core</RouterLink></li>
+        </ul>
+      </li>
+      <li><a href="/">Cerrar Sesion</a></li>
+    </ul>
+  </nav>
+</template>
