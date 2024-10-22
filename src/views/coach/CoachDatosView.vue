0c233f23b029563d574e3bf988ba2406aad445a8
Yhiazer <b.urrea01@ufromail.cl>
2024-10-22 12:37:44 -0300

    style(Coach): añadir estilos a CoachDatosView.vue
    Se añaden estilos para la vista CoachDatosView.vue.

--- src/views/coach/CoachDatosView.vue
+++ src/views/coach/CoachDatosView.vue
@@ -59,3 +59,120 @@
     </main>
 
 </template>
+
+
+<style scoped>
+.coach-container {
+    font-family: 'Poppins', sans-serif;
+    background-color: #292828;
+    color: #fff;
+}
+
+.grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+
+.mensaje-inicial {
+    text-align: center;
+
+    h2 {
+        font-size: 2em;
+        margin-bottom: 10px;
+        margin-top: 20px;
+    }
+}
+
+.boton {
+    margin-left: 30px;
+}
+
+.coach-virtual {
+    text-align: center;
+    padding: 20px;
+    margin: 8px auto;
+    width: 90%;
+    max-width: 1000px;
+    background-color: #212121;
+    color: white;
+    border-radius: 10px;
+}
+
+.coach-virtual h2 {
+    font-size: 2em;
+    margin-bottom: 10px;
+}
+
+.coach-virtual p {
+    margin-bottom: 30px;
+    font-size: 1.2em;
+}
+
+/* Estilos para las opciones (peso, sexo, altura) */
+.options {
+    display: flex;
+    justify-content: space-between;
+    gap: 40px;
+}
+
+.option {
+    background-color: #2c2c2c;
+    padding: 20px;
+    border-radius: 10px;
+    width: 30%;
+    text-align: center;
+}
+
+.option img {
+    width: 100%;
+    margin-bottom: 10px;
+}
+
+.option label {
+    display: block;
+    margin-bottom: 10px;
+    font-size: 1em;
+}
+
+.option input[type="number"] {
+    width: 70%;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    font-size: 1em;
+    margin-top: 10px;
+    background-color: #3c3c3c;
+    color: white;
+}
+
+/* Estilo para las opciones de sexo */
+.sexo-options {
+    display: inline-flex;
+    align-content: center;
+    justify-content: space-around;
+}
+
+.sexo-options input[type="radio"] {
+    margin-right: 5px;
+}
+
+.sexo-options label {
+    font-size: 1em;
+    margin: 0 10px 0 5px
+}
+
+.boton-continuar{
+    background-color: #d22;
+    color: white;
+    border: none;
+    padding: 15px 30px;
+    font-size: 1.2em;
+    border-radius: 10px;
+    cursor: pointer;
+    margin-top: 30px;
+}
+
+.boton-continuar:hover{
+    background-color: #c00;
+}
+</style>
