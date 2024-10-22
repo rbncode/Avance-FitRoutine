db6262470c4da76cdb3622901f54e6439f546fec
Robin <105683782+rbncode@users.noreply.github.com>
2024-10-22 19:38:09 -0300

    feat(adminlistarutinas): agrega html
    Se agrega el HTML en template para vista de rutinas

--- src/views/admin/AdminListaRutinas.vue
+++ src/views/admin/AdminListaRutinas.vue
@@ -44,3 +44,40 @@ export default {
   },
 };
 </script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="admin-rutina-header">
+        <h2>Rutinas</h2>
+      </div>
+      <div class="routine-info">
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Descripción</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="rutina in rutinas" :key="rutina.id">
+              <td>{{ rutina.nombre }}</td>
+              <td>{{ rutina.descripcion }}</td>
+              <td>
+                <button @click="editarRutina(rutina.id)" class="edit-btn">
+                  Editar</button
+                ><button @click="eliminarRutina(rutina.id)" class="delete-btn">
+                  Eliminar
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <button @click="goTo('/admin-nueva-rutina')" class="edit-btn">
+        Agregar Rutina
+      </button>
+    </div>
+  </main>
+</template>
