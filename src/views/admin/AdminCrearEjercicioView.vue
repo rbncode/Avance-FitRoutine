e43e6e5a393f2605d3285f07d2f4e4041d1c3686
Yhiazer <b.urrea01@ufromail.cl>
2024-10-22 13:35:57 -0300

    style(Admin): añadir estilos a AdminCrearEjercicio
    Se añaden estilos para la vista AdminCrearEjercicioView.vue.

--- src/views/admin/AdminCrearEjercicioView.vue
+++ src/views/admin/AdminCrearEjercicioView.vue
@@ -48,3 +48,175 @@
         </form>
     </div>
 </template>
+
+<style scoped>
+.container {
+    margin-top: 20px;
+    margin-left: 270px;
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 60px;
+    background-color: #0D0E16;
+}
+
+form {
+    background-color: #ffffff;
+    color: #000000;
+    border-radius: 10px;
+    padding: 2% 4%;
+    width: 900px;
+    height: auto;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.form-header {
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+}
+
+.form-row {
+    display: flex;
+    justify-content: space-between;
+    gap: 40px;
+}
+
+form input {
+    flex: 1;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    width: 100%;
+    font-size: 16px;
+}
+
+form select {
+    flex: 1;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    width: 100%;
+    font-size: 16px;
+    background-color: #ffffff;
+    color: #000000;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+}
+
+textarea {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+    font-family: inherit;
+    resize: vertical;
+    height: 100px;
+}
+
+.peso-recomendado {
+    display: flex;
+    align-items: center;
+}
+
+.peso-input {
+    width: 70%;
+    -moz-appearance: textfield;
+    appearance: textfield;
+}
+
+.peso-input::-webkit-outer-spin-button,
+.peso-input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+.peso-recomendado input[type="checkbox"] {
+    transform: scale(1.2);
+}
+
+.pc-label {
+    margin-left: 10px;
+}
+
+.valores-def {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    gap: 10px;
+}
+
+.left {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    flex: 1;
+}
+
+.right {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    margin-left: 15px;
+
+    h3 {
+        text-align: left;
+    }
+}
+
+.selector-archivo {
+    max-height: 45px;
+}
+
+.preview {
+    width: 200px;
+    height: 200px;
+    background-color: #f0f0f0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px dashed #ccc;
+    border-radius: 8px;
+}
+
+.buttons {
+    display: flex;
+    justify-content: space-between;
+}
+
+.cancel-button,
+.save-button {
+    padding: 10px;
+    font-size: 18px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 45%;
+}
+
+.save-button {
+    background-color: #D32F2F;
+    color: #fff;
+}
+
+.save-button:hover {
+    background-color: #b71c1c;
+}
+
+.cancel-button {
+    background-color: #e0e0e0;
+    color: #000;
+}
+
+.cancel-button:hover {
+    background-color: #bdbdbd;
+}
+</style>
