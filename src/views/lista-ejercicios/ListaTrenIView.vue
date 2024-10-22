ad3f18aedc600c869c72e15eba887ec8ec7e9ace
Yhiazer <b.urrea01@ufromail.cl>
2024-10-22 13:54:36 -0300

    feat(lista-ejercicio): actualizar ListaTrenIView
    Se completo la estructura de template y las funcionalidades en la vista `ListaTrenIView.vue`.

--- src/views/lista-ejercicios/ListaTrenIView.vue
+++ src/views/lista-ejercicios/ListaTrenIView.vue
@@ -1,3 +1,44 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import BotonGris from '@/components/BotonGris.vue';
+
+const ejercicios = ref([]);
+
+const cargarEjercicios = async () => {
+    try {
+        const res = await axios.get('http://localhost:3000/ejercicios');
+        // Filtro
+        ejercicios.value = res.data.filter(ejercicio => ejercicio.grupoMuscular === 'Tren Inferior');
+    } catch (error) {
+        console.error('Error al cargar los ejercicios:', error);
+    }
+};
+
+onMounted(cargarEjercicios);
+</script>
+
 <template>
-    <p>Lista de ejercicios tren inferior</p>
+
+    <main class="ejercicios-container">
+        <!-- Contenido Principal -->
+        <section class="main-content">
+            <h2>Tren Inferior</h2>
+            <p>Ejercicios que desarrollan fuerza y estabilidad en las piernas y glúteos, trabajando músculos como
+                cuádriceps, isquiotibiales, aductores y pantorrillas, con movimientos como sentadillas, estocadas y peso
+                muerto.</p>
+
+            <!-- Lista ejercicios -->
+            <div class="lista">
+                <div v-for="ejercicio in ejercicios" :key="ejercicio.id" class="ejercicio">
+                    <img :src="ejercicio.imagen" :alt="ejercicio.nombre">
+                    <div>
+                        <h3>{{ ejercicio.nombre }}</h3>
+                        <p>{{ ejercicio.descripcion }}</p>
+                        <BotonGris :link="`/DetalleEjercicio/${ejercicio.id}`" button-text="Detalles" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
