5348212159d0557b56a8ee23718b97de619098e8
Yhiazer <b.urrea01@ufromail.cl>
2024-10-22 13:57:13 -0300

    feat(lista-ejercicio): actualizar ListaTrenSView
    Se completo la estructura de template y las funcionalidades en la vista `ListaTrenSView.vue`.

--- src/views/lista-ejercicios/ListaTrenSView.vue
+++ src/views/lista-ejercicios/ListaTrenSView.vue
@@ -1,3 +1,41 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import BotonGris from '@/components/BotonGris.vue';
+
+const ejercicios = ref([]);
+
+const cargarEjercicios = async () => {
+    try {
+        const res = await axios.get('http://localhost:3000/ejercicios');
+        // Filtro
+        ejercicios.value = res.data.filter(ejercicio => ejercicio.grupoMuscular === 'Tren Superior');
+    } catch (error) {
+        console.error('Error al cargar los ejercicios:', error);
+    }
+};
+
+onMounted(cargarEjercicios);
+</script>
+
 <template>
-    <p>Lista de ejercicios tren superior</p>
+    <main class="ejercicios-container">
+        <!-- Contenido Principal -->
+        <section class="main-content">
+            <h2>Tren Superior</h2>
+            <p>Ejercicios dirigidos a fortalecer y tonificar los músculos de la parte superior del cuerpo...</p>
+
+            <!-- Lista ejercicios -->
+            <div class="lista">
+                <div v-for="ejercicio in ejercicios" :key="ejercicio.id" class="ejercicio">
+                    <img :src="ejercicio.imagen" :alt="ejercicio.nombre" height="200px">
+                    <div>
+                        <h3>{{ ejercicio.nombre }}</h3>
+                        <p>{{ ejercicio.descripcion }}</p>
+                        <BotonGris :link="`/DetalleEjercicio/${ejercicio.id}`" button-text="Detalles" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
