3ef40d5ac085fe2bb3801ec90528cc4983fe4864
Yhiazer <b.urrea01@ufromail.cl>
2024-10-22 13:21:04 -0300

    feat(Admin): actualizar vista AdminListaEjercicio
    Se actualizo la estructura de template en la vista `AdminListaEjercicio.vue`.

--- src/views/admin/AdminListaEjercicioView.vue
+++ src/views/admin/AdminListaEjercicioView.vue
@@ -1,3 +1,34 @@
 <template>
-    <p>Admin lista de ejercicios</p>
+
+    <main>
+        <section class="content">
+            <div class="exercise-table">
+                <h1>Ejercicios</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Sector Muscular</th>
+                            <th>Peso Recomendado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr >
+                            <td>nombre ejercicio</td>
+                            <td>grupo muscular</td>
+                            <td>peso recomendado</td>
+                            <td>
+                                <button class="btn editar">
+                                    <i class='bx bx-edit'></i>Editar
+                                </button>
+                                <button class="btn eliminar" ><i
+                                        class='bx bx-trash'></i>Eliminar</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </main>
 </template>
