6e0d33ebde4f6b26eded0bd4f59cf41f9cc9e720
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 05:29:26 -0300

    style: aplicar estilos a los formularios de edición y visualización
    Se aplican estilos al formulario de edición de usuario y a la sección de información del usuario, incluyendo diseño responsivo y mejoras visuales.

--- src/views/AdminEdicionUsuariosViews.vue
+++ src/views/AdminEdicionUsuariosViews.vue
@@ -86,5 +86,139 @@ export default {
 
 
 
+<style scoped>
+.container {
+    font-family: 'Poppins', sans-serif;
+    color: #ffffff;
+    background: #0D0E16;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
 
+
+.icono i {
+    font-size: 90px;
+    color: #fff;
+}
+.container {
+    margin-left: 270px;
+    flex-grow: 1;
+    padding: 60px;
+    background-color: #0D0E16;
+}
+
+.info-edit-container {
+    display: flex;
+    gap: 20px;
+}
+
+.info-box,
+.edit-box {
+    background-color: #ffffff;
+    color: #000000;
+    padding: 20px;
+    border-radius: 10px;
+    flex: 1;
+    min-width: 0;
+}
+
+.info-box {
+    display: flex;
+    flex-direction: column;
+}
+
+
+.info-box p {
+    font-size: 18px;
+    margin-bottom: 20px;
+}
+
+.info-header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+.info-header i {
+    font-size: 150px;
+    margin-bottom: 10px;
+}
+
+.info-header h2 {
+    font-size: 24px;
+    margin: 0;
+}
+
+.edit-box h2 {
+    font-size: 24px;
+    margin: 0;
+    text-align: center;
+}
+
+h2 {
+    margin-bottom: 20px;
+    font-size: 22px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+form label {
+    margin-bottom: 5px;
+}
+
+form input {
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 2px solid #000;
+    border-radius: 5px;
+    background-color: #ffffff;
+    color: #000000;
+}
+
+.buttons {
+    display: flex;
+    justify-content: space-between;
+}
+
+.cancel-button,
+.save-button {
+    padding: 10px;
+    font-size: 18px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 45%;
+}
+
+.save-button {
+    background-color: #D32F2F;
+    color: #fff;
+}
+
+.save-button:hover {
+    background-color: #b71c1c;
+}
+
+.cancel-button {
+    background-color: #e0e0e0;
+    color: #000;
+}
+
+.cancel-button:hover {
+    background-color: #bdbdbd;
+}
+
+.info-box .info-field {
+    margin-bottom: 15px;
+}
+
+.info-box .info-field:last-of-type {
+    margin-bottom: 0;
+}
+</style>
 
