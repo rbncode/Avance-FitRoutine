f70cd4a275a213920e49846d4514c18e6ab80c49
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 05:46:13 -0300

    feat: agregar template para edición de cuenta
    Se agrega la estructura HTML para el formulario de edición de cuenta.

---
+++ src/views/EdicionUsuarioCuentaViews.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="wrapper">
+            <form @submit.prevent="guardarCambios">
+                <h1>Edicion Cuenta </h1>
+                <div class="input-box">
+                    <h4>Nombre</h4>
+                    <input type="text" v-model="usuario.nombre" required>
+                </div>
+
+                <div class="input-box">
+                    <h4>Correo Electrónico</h4>
+                    <input type="email"  v-model="usuario.email" required>
+                </div>
+                <div class="input-box">
+                    <h4>Edad</h4>
+                    <input type="number"  v-model="usuario.edad" required>
+                </div>
+                <div class="input-box">
+                    <h4>Contraseña</h4>
+                    <input type="password"  v-model="usuario.password" required>
+                </div>
+                <button type="submit" class="btn">Guardar cambios</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+
