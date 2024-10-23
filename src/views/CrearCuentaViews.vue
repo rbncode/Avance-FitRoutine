e433eb132cd9695d5feec56b222b70211b99ac94
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 05:42:07 -0300

    feat: agregar template para crear cuenta
    Se añade la estructura HTML para el formulario de creación de cuenta.

---
+++ src/views/CrearCuentaViews.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div class="container">
+        <div class="wrapper">
+            <form @submit.prevent="guardarUsuario">
+                <h1>Crear cuenta</h1>
+                <div class="input-box">
+                    <h4>Nombre</h4>
+                    <input type="text" v-model="nuevoUsuario.nombre" required>
+                </div>
+
+                <div class="input-box">
+                    <h4>Correo Electrónico</h4>
+                    <input type="email"  v-model="nuevoUsuario.email" required>
+                </div>
+                <div class="input-box">
+                    <h4>Edad</h4>
+                    <input type="number" v-model="nuevoUsuario.edad" required>
+                </div>
+                <div class="input-box">
+                    <h4>Contraseña</h4>
+                    <input type="password"  v-model="nuevoUsuario.password" required>
+                </div>
+                <button type="submit" class="btn">Crear cuenta</button>
+            </form>
+        </div>
+    </div>
+
+</template>
+
+
