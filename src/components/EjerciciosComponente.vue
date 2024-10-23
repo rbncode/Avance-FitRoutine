4e125a461efa2ba3ef69d2b8dc1dfaad6f33a86e
Yhiazer <b.urrea01@ufromail.cl>
2024-10-23 13:03:30 -0300

    refactor(principal): se actualizan las rutas
    Se actualizan las rutas al formato del proyecto actual, usando router-link.

--- src/components/EjerciciosComponente.vue
+++ src/components/EjerciciosComponente.vue
@@ -2,20 +2,20 @@
     <section class="exercises">
         <h2>Descubre tus ejercicios</h2>
         <div class="exercise-grid">
-            <a href="../lista_core/index.html" class="exercise-item">
+            <router-link to="/ListaCore" class="exercise-item">
                 <h3>Core</h3>
                 <img src="../views/img/core.jpeg" alt="Ejercicio de Core">
-
-            </a>
-            <a href="../lista_tren_inferior/index.html" class="exercise-item">
+            </router-link>
+            
+            <router-link to="/ListaTrenI" class="exercise-item">
                 <h3>Tren Inferior</h3>
                 <img src="../views/img/tren-infeior.jpg" alt="Ejercicio de Tren Inferior">
-
-            </a>
-            <a href="../lista_tren_superior/index.html" class="exercise-item">
+            </router-link>
+            
+            <router-link to="/ListaTrenS" class="exercise-item">
                 <h3>Tren Superior</h3>
                 <img src="../views/img/tren-superior.jpg" alt="Ejercicio de Tren Superior">
-            </a>
+            </router-link>
         </div>
     </section>
 </template>
