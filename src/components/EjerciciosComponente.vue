4819e0381b26c0b8839c3b641fe95341487a4510
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 01:31:31 -0300

    feat: añadir template del componente de ejercicios
    Se añadió la estructura HTML del componente  para mostrar una parte de ejercicios.

---
+++ src/components/EjerciciosComponente.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="exercises">
+        <h2>Descubre tus ejercicios</h2>
+        <div class="exercise-grid">
+            <a href="../lista_core/index.html" class="exercise-item">
+                <h3>Core</h3>
+                <img src="../views/img/core.jpeg" alt="Ejercicio de Core">
+
+            </a>
+            <a href="../lista_tren_inferior/index.html" class="exercise-item">
+                <h3>Tren Inferior</h3>
+                <img src="../views/img/tren-infeior.jpg" alt="Ejercicio de Tren Inferior">
+
+            </a>
+            <a href="../lista_tren_superior/index.html" class="exercise-item">
+                <h3>Tren Superior</h3>
+                <img src="../views/img/tren-superior.jpg" alt="Ejercicio de Tren Superior">
+            </a>
+        </div>
+    </section>
+</template>
+
+
