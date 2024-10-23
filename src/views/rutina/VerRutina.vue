5aa6edfa5f643a9720578ccadbabab251d8e7696
Robin <105683782+rbncode@users.noreply.github.com>
2024-10-22 23:31:51 -0300

    feat(verrutinas): agrega html
    Se agrega html a la vista de VerRutina

--- src/views/rutina/VerRutina.vue
+++ src/views/rutina/VerRutina.vue
@@ -29,4 +29,36 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <main>
+    <div class="volver">
+      <button class="volver-btn" onclick="history.back()" id="back-button">
+        Volver
+      </button>
+    </div>
+    <h1 style="text-align: center">RUTINAS</h1>
+    <h2 style="text-align: center">Nombre de Rutina</h2>
+    <div class="info-rutina">
+      <table>
+        <thead>
+          <tr>
+            <th>Ejercicio</th>
+            <th>Peso</th>
+            <th>Repeticiones</th>
+            <th>Series</th>
+            <th>Descanso</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="ejercicio in rutina.ejercicios" :key="ejercicio.id">
+            <td>{{ ejercicio.nombre }}</td>
+            <td>{{ ejercicio.pesoRecomendado }}</td>
+            <td>{{ ejercicio.repeticiones }}</td>
+            <td>{{ ejercicio.series }}</td>
+            <td>{{ ejercicio.descanso }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
+</template>
