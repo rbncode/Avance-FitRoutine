89d8311cede934f7b433a18953cf52f2f8c7103e
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 01:39:53 -0300

    feat: añadir template de la navbar
    Se añadió la estructura HTML de la navbar para la aplicación FitRoutine.

---
+++ src/components/NavBarAdmin.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="navbar">
+        <div class="logo">
+            <h1>FitRoutine</h1>
+        </div>
+        <ul class="nav-links">
+            
+        </ul>
+    </nav>
+
+</template>
+
