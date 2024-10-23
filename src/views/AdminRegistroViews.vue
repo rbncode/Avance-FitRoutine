5ca6f5674a888ef41a0d5c44e260e5fa8a1e388e
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 05:40:09 -0300

    style: agregar estilos para el formulario de registro
    Se añaden los estilos CSS para darle un formato adecuado al formulario de registro.

--- src/views/AdminRegistroViews.vue
+++ src/views/AdminRegistroViews.vue
@@ -58,3 +58,101 @@ export default {
 </script>
 
 
+
+<style scoped>
+.container {
+    margin-left: 270px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 60px;
+    background-color: #0D0E16;
+    margin-top: 50px;
+
+}
+
+.container h2 {
+    font-size: 40px;
+
+}
+
+form {
+    background-color: #ffffff;
+    color: #000000;
+    border-radius: 10px;
+    padding: 3%;
+    width: 800px;
+    height: auto;
+    margin: 50px auto;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.form-header {
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+    color: #ffffff;
+    margin-bottom: 5px;
+    margin-top: -20px;
+}
+
+.form-row {
+    display: flex;
+    justify-content: space-between;
+    gap: 15px;
+}
+
+form input[type="password"] {
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+    width: 100%;
+}
+
+form input {
+    flex: 1;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    width: 100%;
+    font-size: 16px;
+}
+
+.buttons {
+    display: flex;
+    justify-content: space-between;
+}
+
+.cancel-button,
+.save-button {
+    padding: 10px;
+    font-size: 18px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 45%;
+}
+
+.save-button {
+    background-color: #D32F2F;
+    color: #fff;
+}
+
+.save-button:hover {
+    background-color: #b71c1c;
+}
+
+.cancel-button {
+    background-color: #e0e0e0;
+    color: #000;
+}
+
+.cancel-button:hover {
+    background-color: #bdbdbd;
+}
+</style>
