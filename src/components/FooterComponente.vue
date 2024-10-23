a7879afc5e83a2acdaf97ed2d1b1567a5c0ed181
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 01:34:27 -0300

    feat: añadir template del footer
    Se añadió la estructura HTML del footer para la aplicación FitRoutine.

---
+++ src/components/FooterComponente.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <head>
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    </head>
+    
+    <footer>
+        <div class="footer-content">
+            <div class="footer-title">
+                <h3 class="logo-footer">FitRoutine</h3>
+                <p>Únete a nuestra comunidad y alcanza tus objetivos fitness con nosotros</p>
+                <div class="footer-info">
+                    <div class="footer-item">
+                        <i class='bx bxs-location-plus'></i>
+                        <p>Los patos #123</p>
+                    </div>
+                    <div class="footer-item">
+                        <i class='bx bxl-gmail'></i>
+                        <p>fitroutine@gmail.com</p>
+                    </div>
+                    <div class="footer-item">
+                        <i class='bx bxs-phone-call'></i>
+                        <p>+56912345678</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
