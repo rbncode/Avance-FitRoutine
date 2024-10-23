1676822795fedbba855041c9e08e427db7ce0e6b
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 05:38:49 -0300

    feat: agregar template del formulario de registro de usuario
    Se añade la estructura HTML del formulario de registro de usuario.

---
+++ src/views/AdminRegistroViews.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h2 class="form-header">Registro Nuevo Usuario</h2>
+        <form @submit.prevent="guardarUsuario">
+            <div class="form-row">
+                <input type="text" placeholder="Nombre" v-model="nuevoUsuario.nombre" required>
+                <input type="number" placeholder="Edad" v-model="nuevoUsuario.edad" required>
+                <input type="email" placeholder="Correo Electrónico" v-model="nuevoUsuario.email" required>
+            </div>
+
+            <input type="password" placeholder="Contraseña" v-model="nuevoUsuario.password" required>
+
+            <div class="buttons">
+                <button type="button" class="cancel-button" @click="cancelar">Cancelar</button>
+                <button type="submit" class="save-button">Guardar</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+
