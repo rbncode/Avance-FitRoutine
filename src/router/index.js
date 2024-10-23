e78c7cc56754a5e28893d360d2b052a0854a0233
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 06:06:50 -0300

    refactor: actualizar las rutas en el archivo de router
    Se actualizaron las rutas del archivo de router para agregar las vistas de Login, Principal, Crear Cuenta, Perfil de Usuario, Edición de Usuario, Admin Registro, Lista de Usuarios, y Edición de Usuarios Administrador.

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginViews from '@/views/LoginViews.vue'
+import PrincipalViews from '@/views/PrincipalViews.vue'
+import CrearCuentaViews from '@/views/CrearCuentaViews.vue'
+import PerfilUsuarioViews from '@/views/PerfilUsuarioViews.vue'
+import EdicionUsuarioCuentaViews from '@/views/EdicionUsuarioCuentaViews.vue'
+import AdminRegistroViews from '@/views/AdminRegistroViews.vue'
+import AdminListaUsuarios from '@/views/AdminListaUsuarios.vue'
+import AdminEdicionUsuariosViews from '@/views/AdminEdicionUsuariosViews.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'Principal',
+      component: PrincipalViews
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: LoginViews
+    },
+    {
+      path: '/CrearCuenta',
+      name: 'CrearCuenta',
+      component: CrearCuentaViews
+    },
+    {
+      path: '/PerfilUsuario',
+      name: 'perfilUsuario',
+      component: PerfilUsuarioViews
+    },
+    {
+      path: '/EdicionUsuario/:id',
+      name: 'EdicionUsuario',
+      component: EdicionUsuarioCuentaViews
+    },
+    
+    {
+      path: '/AdminRegistro',
+      name: 'AdminRegistro',
+      component: AdminRegistroViews
+    },
+    {
+      path: '/AdminListaUsuarios',
+      name: 'AdminListaUsuarios',
+      component: AdminListaUsuarios
+    },
+    {
+      path: '/AdminEdicionUsuarios/:id',
+      name: 'AdminEdicionUsuarios',
+      component: AdminEdicionUsuariosViews
+    
+    }
+
+
+  ]
 })
 
 export default router
