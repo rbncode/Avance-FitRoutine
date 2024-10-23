66f53aedd0897d296af38119e66a1ae2766fc322
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 01:45:53 -0300

    style: añadir estilos a la barra lateral
    Se añadieron los estilos CSS para la barra lateral del administrador.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -46,3 +46,106 @@ export default {
     </nav>
 </template>
 
+<style scoped>
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    font-weight: lighter;
+}
+
+.menu-dashboard {
+    width: 250px;
+    height: calc(100vh - 60px);
+    background: #0D0E16;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    border-right: 2px solid #ffffff;
+    position: fixed;
+    top: 58px;
+    left: 0;
+}
+
+
+.top-menu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-bottom: 20px;
+
+}
+
+.icono i {
+    font-size: 80px;
+    color: #fff;
+}
+
+.admin-name {
+    font-size: 18px;
+    color: #fff;
+    margin-top: 10px;
+}
+
+.admin-email {
+    font-size: 14px;
+    color: #b0b0b0;
+}
+
+.menu {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    flex-grow: 1;
+    margin-top: 0px;
+}
+
+.enlace {
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    color: #fff;
+    padding: 10px 15px;
+    border-radius: 8px;
+    text-decoration: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.enlace:hover {
+    background-color: #2a2b38;
+}
+
+.enlace i {
+    margin-right: 10px;
+    font-size: 20px;
+}
+
+.logout-button {
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    color: #fff;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    padding: 10px 15px;
+    border-radius: 8px;
+    transition: background-color 0.3s ease;
+    margin-top: auto;
+}
+
+.logout-button i {
+    margin-right: 10px;
+    font-size: 20px;
+}
+
+.logout-button:hover {
+    background-color: #2a2b38;
+}
+</style>
