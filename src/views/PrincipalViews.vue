8b71acd5cfc553e463368ae1141ecb8fe3a8c771
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 06:03:07 -0300

    feat: añadir plantilla de la página de inicio
    Se añadió la plantilla HTML para la sección principal de bienvenida y la misión de la aplicación FitRoutine, incluyendo un botón de inicio y un componente de ejercicios.

---
+++ src/views/PrincipalViews.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="hero">
+        <div class="hero-text">
+            <h1>
+                <span class="line1">Bienvenido</span>
+                <span class="line2">a FitRoutine</span>
+            </h1>
+
+            <RouterLink to="/Login">
+                <button class="btn">Comenzar</button>
+            </RouterLink>
+        </div>
+    </section>
+
+    <section class="mission">
+        <div class="mission-content">
+            <img src="../views/img/mision.jpg" alt="Imagen de entrenamiento" class="mission-img">
+            <div class="mission-text">
+                <h2>Nuestra Misión</h2>
+                <p>A medida que más personas se inscriben en el gimnasio, se vuelve fundamental contar con un plan de
+                    entrenamiento para mejorar cada día y seguir una rutina de manera ordenada.</p>
+                <p>Un plan bien estructurado no solo maximiza los resultados, sino que también ayuda a mantener la
+                    motivación y la constancia en el camino hacia tus objetivos fitness.</p>
+            </div>
+        </div>
+    </section>
+    <EjerciciosComponente />
+
+</template>
+
+
