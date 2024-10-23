90e25ca6215ff456a30b89d5f79c3f8d8e690bb7
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 01:44:24 -0300

    feat: añadir lógica de navegación a la barra lateral
    Se añadió la lógica de navegación para la barra lateral.

---
+++ src/components/SideBar.vue
@@ -0,0 +1,17 @@
+<script>
+import { useRouter } from 'vue-router';
+
+export default {
+    data() {
+        return {
+            router: useRouter()
+        };
+    },
+    methods: {
+        goTo(ruta) {
+            this.router.push(ruta);
+        }
+    }
+}
+</script>
+
