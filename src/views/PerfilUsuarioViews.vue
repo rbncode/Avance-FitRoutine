50920e3963f85b34f1beb0e2acbd4d39bfd0bf98
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 05:57:05 -0300

    feat: añadir plantilla de perfil de usuario
    e añadió la plantilla HTML para mostrar la información del perfil del usuario, incluyendo su nombre, correo, contraseña y edad.

---
+++ src/views/PerfilUsuarioViews.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <main>
+            <section class="profile">
+                <h1>Perfil Usuario</h1>
+                <div class="profile-card">
+                    <div class="user-icon">
+                        <i class='bx bxs-user-circle'></i>
+                    </div>
+                    <div class="profile-info">
+                        <p><strong>Nombre</strong>: {{ usuario.nombre }}</p>
+                        <p><strong>Correo Electronico</strong>: {{ usuario.email }}</p>
+                        <p><strong>Password</strong>: {{ usuario.password }}</p>
+                        <p><strong>Edad</strong>:{{ usuario.edad }}</p>
+                    </div>
+                    <button class="edit-button" @click="editarPerfil">Editar perfil</button>
+            </div>
+        </section>
+    </main>
+</div>
+</template>
+
