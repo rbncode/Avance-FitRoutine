098553f3cdd24767b43ea99512dbb5505dc6469a
C-RB <134893101+C-RB@users.noreply.github.com>
2024-10-23 06:14:59 -0300

    refactor: modificar script para gestionar rutas de admin y usuario
    Se añadió useRoute() para verificar dinámicamente si la ruta actual pertenece a un administrador. También se importaron los componentes necesarios para gestionar las vistas de administrador y usuario.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { useRoute } from 'vue-router';
+import { RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
+import NavBarAdmin from './components/NavBarAdmin.vue';
+import Footer from './components/FooterComponente.vue';
+import SideBar from './components/SideBar.vue';
+
+const route = useRoute();
+
+const esAdmin = () => {
+  return route.path.includes('/Admin');
+};
 </script>
 
 <template>
